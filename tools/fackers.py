99f9c1b0216b36b39a9d205518c542ba3c28d879
n.rumyancev <n.rumyancev@dssl.ru>
2026-05-03 15:46:31 +0300

    .

--- tools/fackers.py
+++ tools/fackers.py
@@ -1,14 +1,9 @@
 import random
 import time
 
-email_subdomen = ["gmail.com", "mail.ru", "internet.ru", "bk.ru", "inbox.ru"]
 
+class Fakers:
+    email_subdomen = ["gmail.com", "mail.ru", "internet.ru", "bk.ru", "inbox.ru"]
 
-def get_random_email_for_user() -> str:
-    return f"user_{time.time()}@{random.choice(email_subdomen)}"
-
-
-
-
-
-
+    def get_random_email_for_user(self) -> str:
+        return f"user_{time.time()}@{random.choice(Fakers.email_subdomen)}"
