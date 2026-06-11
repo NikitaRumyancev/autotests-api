6b313bb2c94315810f4ba2a42e44f846d37532c7
NikitaRumyancev <nik.rum2003@mail.ru>
2026-06-11 15:53:21 +0300

    comment incoorect str

--- pydantic_basics.py
+++ pydantic_basics.py
@@ -75,7 +75,7 @@ print(model1.model_dump_json(by_alias=True))
 #
 # Windows, 11
 # 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)]
-import sys
-
-
-print(sys.version)
+# import sys
+#
+#
+# print(sys.version)
